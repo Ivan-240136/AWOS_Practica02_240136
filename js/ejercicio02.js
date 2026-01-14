a8d3f32c053ee95531851f0e34f3cd1d5340b3fb
Ivan-240136 <240136@utxicotepec.edu.mx>
2026-01-14 09:49:22 -0600

    Practica 02
    update: Se actualizó el código del Ejercicio 02: Tipos de datos

--- js/ejercicio02.js
+++ js/ejercicio02.js
@@ -142,3 +142,140 @@ console.log(`Las variables declaradas son del tipo:
      typeProperty : ${typeof (typeProperty)}
      addressProperty : ${typeof (addressProperty)}
      `);
+
+    /* Existen métodos para manipular los datos de tipo STRING, como por ejemplo:*/
+    /* El método toUpperCase que convierte todas las letras en mayúsculas*/
+    console.log(`El usuario logeado es: ${usuarioLogeado.toUpperCase()}`);
+    /* Por lo contrario en método toLowerCase que convierte todas las letras en minúsculas*/
+    console.log(`Descripción de la propiedad es: ${descriptionProperty.toLowerCase()}`);
+    /* También existe la propiedad que cuenta el número de caracteres que forman el texto, llamado length */
+    console.log(`El nombre de la propiedad tiene un total de ${nameProperty.length}.`);
+    /* Tenemos un método que nos permite hacer subcadenas o extraer la porción de la palabra en base a su posicionamiento*/
+    let lastName = fullnameOwner.slice(8, fullnameOwner.length);
+    console.log(`El apellido del usuario es: ${lastName}`);
+
+    /* Otra función útil es aquella que nos permite reemplazar palabras o caracteres dentro de una cadena de texto, llamado replace */
+    console.log(`Reemplazo de la palabra MAR por RÍO, Descripción de la propiedad: ${descriptionProperty.replace("MAR", "RÍO")}`);
+
+    /* O el reemplazo de todas las ocurrencias de una letra o palabra */
+    console.log(`Reemplazo de todas las letras A por el número 4, Nombre de la propiedad: ${nameProperty.replaceAll("a", "4")}`);
+
+    /* 5. BEGINT (Entero de Alta Precisión), o Alta Amplitud */
+    console.warn("5.- BIGINT (ENTERO DE ALTA PRECISIÓN)");
+    let numeroGrande1 = 1234567890; /*10 Digitos */
+    let numeroGrande2 = 12345678901234567890; /*20 Digitos */
+    let numeroGrande3 = 123456789012345678901234567890; /*30 Digitos */
+    let numeroGrande4 = 1234567890123456789012345678901234567890; /*40 Digitos */
+
+    /* Imprimimos los valores de las variables y observamos que pasa */
+    console.log(`Los valores y tipos de datos son:
+    numeroGrande1 = ${numeroGrande1} | tipo = ${typeof numeroGrande1}
+    numeroGrande2 = ${numeroGrande2} | tipo = ${typeof numeroGrande2}
+    numeroGrande3 = ${numeroGrande3} | tipo = ${typeof numeroGrande3}
+    numeroGrande4 = ${numeroGrande4} | tipo = ${typeof numeroGrande4}
+    `);
+
+    /* Para no perder precisión en estos números JavaScript tiene un tipo de datos especial llamado BIGINT,
+    para poder almacenar el valor en este tipo de dato, solo basta con invocar el método de conversión en la
+    inicialización de la variable */
+    numeroGrande1 = BigInt(1234567890); /*10 Digitos */
+    numeroGrande2 = BigInt(12345678901234567890); /*20 Digitos */
+    numeroGrande3 = BigInt(123456789012345678901234567890); /*30 Digitos */ 
+    numeroGrande4 = BigInt(1234567890123456789012345678901234567890); /*40 Digitos */  
+
+    console.log(`Los valores y tipos de datos son:
+    numeroGrande1 = ${numeroGrande1} | tipo = ${typeof numeroGrande1}
+    numeroGrande2 = ${numeroGrande2} | tipo = ${typeof numeroGrande2}
+    numeroGrande3 = ${numeroGrande3} | tipo = ${typeof numeroGrande3}
+    numeroGrande4 = ${numeroGrande4} | tipo = ${typeof numeroGrande4}
+    `);
+
+    /* 6. SYMBOL */
+    /* Este tipo de datos a parte de almacenar el valor, almacena la dirección física en memoria donde se almacena el valor,
+    logrando que todos los valores de cada variable siempre sean ÚNICOS */
+    console.warn("6.- SYMBOL (SÍMBOLOS)");
+    const numero1 = 3 ;
+    const numero2 = 3.0 ;
+    const numero3 = "3" ;
+    const numero4 = "3.0" ;
+    const numero5 = Symbol(3) ; 
+    const numero6 = Symbol(3.0) ;
+    const numero7 = Symbol("3") ;
+    const numero8 = Symbol("3.0") ;
+    const numero9 = Symbol(3) ; // Mismo valor que numero5 pero diferente símbolo en memoria
+
+    console.log(`Imprimiendo los valores y tipos de datos:
+        numero1, valor = ${numero1} | tipo = ${typeof (numero1)}
+        numero2, valor = ${numero2} | tipo = ${typeof (numero2)}
+        numero3, valor = ${numero3} | tipo = ${typeof (numero3)}
+        numero4, valor = ${numero4} | tipo = ${typeof (numero4)}
+        numero5, valor = ${numero5.description} | tipo = ${typeof (numero5)}
+        numero6, valor = ${numero6.description} | tipo = ${typeof (numero6)}
+        numero7, valor = ${numero7.description} | tipo = ${typeof (numero7)}
+        numero8, valor = ${numero8.description} | tipo = ${typeof (numero8)}
+    `);
+
+    /* Realizando un par de validaciones */
+    if(numero1 == numero2)
+        console.log("Se comparó el valor de numero1 y numero2 evaluando que son iguales en valor");
+    else
+        console.log("Se comparó el valor de numero1 y numero2 evaluando que son diferentes en valor");
+    if(numero1 == numero3)
+        console.log("Se comparó el valor de numero1 y numero3 evaluando que son iguales en valor");
+    else
+        console.log("Se comparó el valor de numero1 y numero3 evaluando que son diferentes en valor");
+    if(numero1 === numero3)
+        console.log("Se comparó el valor de numero1 y numero3 evaluando que son iguales en valor");
+    else
+        console.log("Se comparó el valor de numero1 y numero3 evaluando que son iguales en valor pero diferente tipo de dato");
+    if(numero1 == numero4)
+        console.log("Se comparó el valor de numero1 y numero4 evaluando que son iguales en valor");
+    else
+        console.log("Se comparó el valor de numero1 y numero4 evaluando que son diferentes en valor");
+    if(numero1 === numero4)
+        console.log("Se comparó el valor de numero1 y numero4 evaluando que son iguales en valor");
+    else
+        console.log("Se comparó el valor de numero1 y numero4 evaluando que son iguales en valor pero diferente tipo de dato");
+    if(numero1 == numero5)
+        console.log("Se comparó el valor de numero1 y numero5 evaluando que son iguales en valor");
+    else
+        console.log("Se comparó el valor de numero1 y numero5 evaluando que son diferentes en valor ");
+    if(numero5 == numero9)
+        console.log("Se comparó el valor de numero5 y numero9 evaluando que son iguales en valor");
+    else
+        console.log("Se comparó el valor de numero5 y numero9 evaluando que son diferentes en valor pero diferente tipo de dato");
+
+    /* 7. NULL (NULO) */
+    /* Este tipo de datos es similar a UNDEFINED, con la diferencial de que los usuarios o el sistema es conciente que se le asignó este valor */
+
+    console.warn("7.- NULL (NULO)");
+
+    let isPremiumUser;
+    let isUserLoged; 
+    var todayDate = new Date();
+    var lastLogin = todayDate;
+    let isNewUser; // El sistema sabe que es un usuario nuevo
+
+    console.log(`Las variables del usuario son:
+        isPremiumUser, valor: ${isPremiumUser} | tipo = ${typeof (isPremiumUser)}
+        isUserLoged, valor: ${isUserLoged} | tipo = ${typeof (isUserLoged)}
+        isNewUser, valor: ${isNewUser} | tipo = ${typeof (isNewUser)}
+    `);
+     
+    /* Durante la navegación del usuario en la plataforma en algún punto ingresa sus credenciales y se valida en base date */    
+    /* Simulando que las credenciales fueron correctas */
+
+    
+    isUserLoged = true;
+    isPremiumUser = null;
+    if(lastLogin == todayDate)
+        isNewUser = true;
+    else
+        isNewUser = false; 
+
+    
+    console.log(`Las variables del usuario son:
+        isPremiumUser, valor: ${isPremiumUser} | tipo = ${typeof (isPremiumUser)}
+        isUserLoged, valor: ${isUserLoged} | tipo = ${typeof (isUserLoged)}
+        isNewUser, valor: ${isNewUser} | tipo = ${typeof (isNewUser)}
+    `);
